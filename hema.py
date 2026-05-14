113491e5512685b45ce1abb58676dcd6be544c06
lenovo <u2864297@uel.ac.uk>
2026-05-14 22:39:36 +0300

    hema.py

--- hema.py
+++ hema.py
@@ -1,118 +1,35 @@
-import os
 import cv2 as cv
 import numpy as np
 
-face_cascade = cv.CascadeClassifier(
-    cv.data.haarcascades + "haarcascade_frontalface_default.xml"
-)
+face_cascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
 
 IMG_SIZE = (128, 128)
 
-# CLAHE object
-clahe = cv.createCLAHE(
-    clipLimit=2.0,
-    tileGridSize=(8, 8)
-)
-
+clahe = cv.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
 def detect_and_preprocess(img):
-
-    # =========================
     # BGR -> YCrCb
-    # =========================
     ycrcb = cv.cvtColor(img, cv.COLOR_BGR2YCrCb)
 
     # split channels
     y, cr, cb = cv.split(ycrcb)
 
-    # =========================
     # Enhance illumination
-    # =========================
     y = clahe.apply(y)
 
-    # =========================
     # Face Detection
-    # =========================
-    faces = face_cascade.detectMultiScale(
-        y,
-        scaleFactor=1.0025,
-        minNeighbors=4,
-        minSize=(50, 50)
-    )
+    faces = face_cascade.detectMultiScale(y, scaleFactor=1.003, minNeighbors=4, minSize=(35, 35))
 
-    # no face found
     if len(faces) == 0:
         return None
 
     # largest face
     x, y1, w, h = max(faces, key=lambda f: f[2] * f[3])
 
-    # crop face from enhanced Y channel
     face = y[y1:y1+h, x:x+w]
 
-    # resize
     face = cv.resize(face, IMG_SIZE)
 
-    # normalize
     face = face.astype(np.float32) / 255.0
 
     return face
-
-
-def load_and_preprocess(path):
-
-    data = []
-    labels = []
-
-    persons = sorted(
-        os.listdir(path),
-        key=lambda x: int(x.replace("person", ""))
-    )
-
-    for label, person in enumerate(persons):
-
-        if not person.startswith("person"):
-            continue
-
-        person_path = os.path.join(path, person)
-
-        for img_name in os.listdir(person_path):
-
-            img_path = os.path.join(person_path, img_name)
-
-            img = cv.imread(img_path)
-
-            if img is None:
-                continue
-
-            face = detect_and_preprocess(img)
-
-            # skip if no face detected
-            if face is None:
-                continue
-
-            data.append(face)
-            labels.append(label)
-
-    return np.array(data), np.array(labels)
-
-
-if __name__ == "__main__":
-
-    dataset_path = "dst"
-
-    data, labels = load_and_preprocess(dataset_path)
-
-    print("Total images:", len(data))
-    print("Sample label:", labels[0])
-    print("Shape:", data[0].shape)
-
-    # display sample
-    display = (data[11]).astype(np.uint8)
-
-    display = cv.resize(display, (600, 600))
-
-    cv.imshow("Face Preprocessed", display)
-
-    cv.waitKey(0)
-    cv.destroyAllWindows()
--- split_dataset.py
+++ split_dataset.py
@@ -1,23 +1,66 @@
 import os
 import shutil
+from collections import defaultdict
 
 source_folder = "./originalimages_part1"
-destination_folder = "dst"
+destination_folder = "splited_dataset"
+
+# نجمع الصور لكل شخص
+people = defaultdict(list)
 
 for img_name in os.listdir(source_folder):
 
-    if not img_name.endswith(".jpg"):
-        continue
-
     person_id = img_name.split("-")[0]
 
-    person_folder = os.path.join(destination_folder, f"person{person_id}")
+    people[person_id].append(img_name)
 
-    os.makedirs(person_folder, exist_ok=True)
+# تقسيم الصور
+for person_id, images in people.items():
 
-    src = os.path.join(source_folder, img_name)
-    dst = os.path.join(person_folder, img_name)
+    # ترتيب الصور
+    images.sort()
 
-    shutil.copy(src, dst)
+    # آخر صورة للـ identification
+    identification_image = images[-1]
 
-print("Done ")
+    # باقي الصور
+    remaining_images = images[:-1]
+
+    # 80% train
+    train_count = round(len(remaining_images) * 0.8)
+
+    train_images = remaining_images[:train_count]
+
+    # 20% test
+    test_images = remaining_images[train_count:]
+
+    # إنشاء الفولدرات
+    train_folder = os.path.join(destination_folder, f"person{person_id}", "train")
+    test_folder = os.path.join(destination_folder, f"person{person_id}", "test")
+    identification_folder = os.path.join(destination_folder, f"person{person_id}", "identification")
+
+    os.makedirs(train_folder, exist_ok=True)
+    os.makedirs(test_folder, exist_ok=True)
+    os.makedirs(identification_folder, exist_ok=True)
+
+    # نسخ train
+    for img in train_images:
+        shutil.copy(
+            os.path.join(source_folder, img),
+            os.path.join(train_folder, img)
+        )
+
+    # نسخ test
+    for img in test_images:
+        shutil.copy(
+            os.path.join(source_folder, img),
+            os.path.join(test_folder, img)
+        )
+
+    # نسخ identification
+    shutil.copy(
+        os.path.join(source_folder, identification_image),
+        os.path.join(identification_folder, identification_image)
+    )
+
+print("Done ✅")
